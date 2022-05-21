93c8174ee417d7b07098b5af97a113f1d49c7bd5
Andrew Gavegan <zzandrewzz@optusnet.com.au>
2022-05-21 11:31:15 +1000

    populated category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,30 +1,83 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+// add tag and product_tag //
+const { Tag, Category, Product, ProductTag } = require('../../models');
 
 // The `/api/categories` endpoint
 // add async to function so we can use await //
-router.get('/',async (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
-
-
+router.get('/', async (req, res) => {
+  // find all categories // be sure to include its associated Products
+  // will use a try catch statement //
+  try {
+    const findCategories = await Catergory.findAll({
+      include: [{ model: Product }],
+    })
+    res.status(200).json(findCategories);
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+// add async to function so we can use await //
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value // be sure to include its associated Products
+  const { id: category_id } = req.params;
+  console.log(category_id);
+    // will use a try catch statement //
+  try {
+    const selectCategory = await Category.findByPk(category_id);
+    res.status(200).json(selectCategory);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// add async to function so we can use await //
+router.post('/', async (req, res) => {
+  // create a new category   // will use a try catch statement //
+  try {
+    const { category_name } = req.body;
+    const newCategory = await Category.create({
+      category_name,
+    })
+    res.status(200).json(newCategory);
+  } catch (err) {
+    res.status(500);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+// add async to function so we can use await //
+router.put('/:id', async (req, res) => {
+  // update a category by its `id` value // use a try catch statement //
+  try {
+    const { id: category_id } = req.params;
+    const { category_name: updatedName } = req.body;
+    console.log(updatedName);
+    const updatedCategory = await Category.update(
+      { categoryName: updatedName },
+      { where: { id: category_id },
+    }
+    )
+    res.status(200).send(`${updatedCategory} has been updated`)
+  } catch (err) {
+    res.status(500);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+// add async to function so we can use await //
+router.delete('/:id', async (req, res) => {
+  // delete a category by its `id` value // use a try catch statement //
+  try {
+    // req.params is whatever is entered in the url parameter after the slash usually //
+    const { id: category_id } = req.params;
+    const delCategory = await Category.destroy({
+      // deleting the category that has the matching id to the one that was entered into the parameter //
+      where: { id: category_id }
+    });
+    res.status(200).json(delCategory);
+  } catch (err) {
+    res.status(500);
+  }
 });
 
 module.exports = router;
